e90646b09bfc6cddb06499f7538f7fa1fe1abf25
Marlon <72989676+marlonmountjoy@users.noreply.github.com>
2025-09-30 21:07:02 -0700

    Add SkipToContent and clean baseline

---
+++ my-react-app/src/components/SkipToContent/SkipToContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SkipToContent.css";
+
+/**Skip to content link */
+
+export default function SkipToContent() {
+    return (
+        <a href="#main-content" className="skip-link">
+            Skip to main content
+        </a>
+    );
+}
