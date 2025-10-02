427fe228512b14d186b800fdcbd659c1e3ddcd4f
Marlon <72989676+marlonmountjoy@users.noreply.github.com>
2025-10-02 13:48:49 -0700

    Updated layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SkipToContent from "./components/SkipToContent/SkipToContent";
 import Header from "./components/Header/Header";
 import TopNav from "./components/TopNav/TopNav";
 import Footer from "./components/Footer/Footer";
@@ -6,12 +7,24 @@ import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
-      <Header />
-      <TopNav />
-      <main id="main-content">
-        <p>***************************************************</p>
-      </main>
-      <Footer />
+      <SkipToContent />
+      <div className="layout">
+        <Header />
+        <TopNav />
+        <main id="mainContent" tabIndex={-1}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas
+            praesentium perferendis temporibus recusandae similique possimus
+            reprehenderit sunt voluptatibus dicta autem? Voluptas pariatur totam
+            minima inventore excepturi soluta laboriosam suscipit reprehenderit!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            officiis eligendi deleniti laborum illo ab in rem, dolore repellat,
+            facere ad nobis fugit dolorem quisquam natus, a nihil et
+            voluptatibus.
+          </p>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
