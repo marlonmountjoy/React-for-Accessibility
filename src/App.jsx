096cf46abadfa7553b69e1a346a62c153b05c4f7
Marlon <72989676+marlonmountjoy@users.noreply.github.com>
2025-10-02 12:41:33 -0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import TopNav from "./components/TopNav/TopNav";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TopNav />
+      <main id="main-content">
+        <p>***************************************************</p>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
